f2e41c476ff551ca8f6a637d6b58adf4a44d668e
linda-20 <lindaaron40@gmail.com>
2024-01-31 11:22:06 +0300

    Completed asssignment

--- src/components/App.js
+++ src/components/App.js
@@ -18,13 +18,25 @@ function Home() {
 
 {/* write an <About> component here */}
 
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+      {/* Additional content for the About component if needed */}
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About/>
+
     </div>
   );
 }
